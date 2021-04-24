c62bd1b6a1bbad4952ff6b435fde55c32ddadc61
Juwita14 <56763570+Juwita14@users.noreply.github.com>
2021-04-23 20:35:38 -0700

    first commit

---
+++ RazorPagesMovies/Data/MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Movies;
+
+namespace RazorPagesMovies.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovies.Movies.Movie> Movie { get; set; }
+    }
+}
---
+++ RazorPagesMovies/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovies.Models;
+using RazorPagesMovies.Movies;
+
+namespace RazorPagesMovies.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovies.Models.MoviesContext _context;
+
+        public IndexModel(RazorPagesMovies.Models.MoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
